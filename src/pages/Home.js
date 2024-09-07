9c06aa5860fc88cfd6669b5126f9c1e592a47296
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-09-07 20:45:47 +0300

    adding home-page

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../styles/home.css";
+import chicken from "../Assets/IMAGES/beef4.png";
+import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <div className="md:home home-mob w-full">
+        <div className="h-[100%] mx-auto max-w-[1380px]">
+              {/* button container */}
+              <div className="flex flex-col md:ml-[50px] ml-[25px] pt-[135px] justify-center items-center w-[100%] h-[50%] pr-[70px]">
+                <h1 className="text-xl md:text-4xl text-white">welcome to <span className="font-bold bg-orange-600 rounded-lg duration-500 px-[5px] text-orange-800 mr-[10px]">Tasty</span>Restaurant</h1>
+                <h1 className="text-xl md:text-4xl text-white">where healthy & delicious Food &</h1>
+                <h1 className="text-xl md:text-4xl text-white ">the best Services and Fast Delivery.</h1>
+                <Link to="/services" className="animate-pulse md:ml-[5px]">
+                  <button className=" text-white text-[15px] md:text-[20px] px-[10px] max-w-[150px] border-[1px] rounded-md md:py-[7px]  text-xl border-orange-600 bg-orange-700 hover:border-none hover:scale-105 duration-500 mt-[30px]">
+                    At Your Service
+                  </button>
+                </Link>
+              </div>
+            </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;

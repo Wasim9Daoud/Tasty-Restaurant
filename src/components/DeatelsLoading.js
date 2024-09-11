8d59acbe1be8e47c0ef5b972569f8503f780f3cb
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-09-11 13:53:17 +0300

    add DetailsLoading component

---
+++ src/components/DeatelsLoading.js
@@ -0,0 +1,20 @@
+
+
+const DeatelsLoading = () => {
+
+  return (
+    <div className="bg-gray-400 w-full h-full">
+      <div className=" max-w-[1380px] mx-auto">
+        <div className=" p-[10px] flex flex-col  justify-center md:items-start items-center pt-[50px] pb-[32px] gap-[30px]">
+          <div className="animate-pulse bg-gray-300 rounded-xl w-[80%] md:w-[50%] h-[350px] text-center flex flex-col justify-center items-center">
+          </div>
+          <div className="flex justify-center w- md:w-[50%]">
+            <button className=" px-[10px] animate-pulse py-[5] bg-gray-300 w-[200px] h-[50px] rounded-md">
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default DeatelsLoading;

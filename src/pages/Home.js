35bb36300c873a5d0521299fc9c0e1acb8c7ec47
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-09-13 14:40:23 +0300

    fix home page ( scroll to top )

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/home.css";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+ useEffect(
+  ()=>{
+    window.scrollTo(0,0)
+  }
+ )
   return (
     <>
       <div className="md:home home-mob">
